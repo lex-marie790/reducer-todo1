35acb18fd83235bf0e3bb1a287f6563177c15494
lex-marie790 <lexi.marie790@gmail.com>
2020-05-26 04:40:26 -0700

    actions file updated

--- todo/src/components/actions.js
+++ todo/src/components/actions.js
@@ -1,39 +1,7 @@
-import React from "react";
-
-const handleChanges = e => {
-    e.preventDefault();
-    this.setState({
-        newItem: e.target.value
-    });
-};
-
-const handleSubmit = e => {
-    e.preventDefault();
-    this.props.addTask(this.state.newItem)
-};
-
-const taskSubmit = e => {
-
-};
-
-const clearCompleted = e => {
-
-};
-
-const toggleItem = e => {
-
-}
-
-const addTask = e => {
-
-}
-
-export default handleChanges;
-export default handleSubmit;
-export default taskSubmit;
-export default clearCompleted;
-export default toggleItem;
-export default addTask;
-
-
+export const ADD_TASK = "ADD-TASK";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
+// export const ADD_TASK = "ADD-TODO";
+// export const ADD_TASK = "ADD-TODO";
+// export const ADD_TASK = "ADD-TODO";
 
